e59b9127b829cfe049afc6a2deefbe7e9b40cac8
Lucas Fraga <lucasfraga.dev@gmail.com>
2025-04-07 22:34:25 -0300

    Ultimo exercício revisão estrutura sequencial

--- src/Exercicios6.java
+++ src/Exercicios6.java
@@ -1,5 +1,17 @@
-pulic class Exercicios6 {
+public class Exercicios6 {
     public static void main(String[] args) {
-        flat
+        double A = 3.0;
+        double B = 4.0;
+        double C = 5.2;
+        double areaTrianguloRetangulo = (A * C) / 2;
+        double areaCirculo  = Math.PI * C * C;
+        double areaTrapezio = ((A + B) * C) / 2;
+        double areaQuadrado = B * B;
+        double areaRetangulo = A * B;
+        System.out.println("Area triangulo retangulo: " + areaTrianguloRetangulo);
+        System.out.println("Area Circulo: " + areaCirculo);
+        System.out.println("Area Trapezio: " + areaTrapezio);
+        System.out.println("Area Quadrado: " + areaQuadrado);
+        System.out.println("Area Retangulo: " + areaRetangulo);
     }
 }
