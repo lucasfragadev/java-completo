0ccf16dba12c1c83658d7fcfc84e5c08b35f2721
LucasAF <lucasaf@ciandt.com>
2025-04-22 19:48:31 -0300

    Exercicios de Estruturas de Repetição com Java

---
+++ src/EstruturaRepeticao/Exercicios02.java
@@ -0,0 +1,37 @@
+package EstruturaRepeticao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicios02 {
+    public static void  main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        double valorX;
+        double valorY;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o valor X: ");
+        valorX = sc.nextDouble();
+        System.out.println("Digite o valor Y: ");
+        valorY = sc.nextDouble();
+
+        while (valorX != 0 && valorY != 0) {
+            if (valorX > 0 && valorY > 0) {
+                System.out.println("Q1");
+            } else if (valorX < 0 && valorY > 0) {
+                System.out.println("Q2");
+            } else if (valorX < 0 && valorY < 0) {
+                System.out.println("Q3");
+            } else {
+                System.out.println("Q4");
+            }
+
+            System.out.println("Digite o valor X: ");
+            valorX = sc.nextDouble();
+            System.out.println("Digite o valor Y: ");
+            valorY = sc.nextDouble();
+        }
+        sc.close();
+   }
+}
