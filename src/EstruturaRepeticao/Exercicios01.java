1c82b6b5cf531665754f416f886db87c9d430d11
LucasAF <lucasaf@ciandt.com>
2025-04-22 19:33:31 -0300

    Exercicios de Estruturas de Repetição com Java

---
+++ src/EstruturaRepeticao/Exercicios01.java
@@ -0,0 +1,23 @@
+package EstruturaRepeticao;
+
+import java.util.Scanner;
+
+public class Exercicios01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nDigite sua senha: ");
+        int senha = sc.nextInt();
+
+        while (senha != 2002) {
+            System.out.println("\nSenha Inválida");
+            System.out.println("\nDigite sua senha: ");
+            senha = sc.nextInt();
+
+            if (senha == 2002) {
+                System.out.println("\nSenha Correta");
+                System.out.println("\nAcesso Permitido");
+                break;
+            }
+        }
+    }
+}
